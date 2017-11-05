bc71a8ef024dcff82198ec539fd9a6ec53b1a3f6
humanalgorithm <scottomj@gmail.com>
2017-11-04 22:16:10 -0400

    working assets

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,8 @@ module InstagramMini
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    #config.assets.enabled = true
+    #config.assets.paths << Rails.root.join("app", "vendor", "bootstrap","css")
+    #config.assets.paths << Rails.root.join("app", "vendor", "bootstrap","js")
   end
 end
